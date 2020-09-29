5b8192804bdb693c94311986b290ebd6d504d892
shivani tangellapally <69983357+shivani-ta@users.noreply.github.com>
2020-09-29 00:30:15 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,27 @@
+function temperatureConverter() {
+    valNum = document.getElementById("tempInput").value
+    console.log(parseInt(valNum),"valueeeee")
+    document.getElementById("outputCelsius").innerHTML =  (valNum-32) / 1.8;
+    event.preventDefault()
+  }
+
+  function getQuote(){
+    var quotes = ['Change the world by being yourself. - Amy Poehler',
+                     'Never regret anything that made you smile. - Mark Twain', 
+                     'Everything you can imagine is real. - Pablo Picasso.',
+                     'So many books, so little time. - Frank Zappa'
+                    ];
+    var  quote = Math.floor(Math.random() * quotes.length);
+    document.querySelector('#quote').innerHTML = quotes[quote];
+}
+
+function storeContact(){
+  name = document.getElementById("nameInput").value
+  email= document.getElementById("emailInput").value
+  name.length&&email.length>0?alert(name+" Thankyou for submitting"):alert("invalid entry");
+}
+
+
+function ajax(){
+ 
+}
