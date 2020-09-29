d2b662b3c6b9a14fde0a3d2ccd9f6f41eaf12d13
shivani tangellapally <69983357+shivani-ta@users.noreply.github.com>
2020-09-29 00:30:44 -0500

    Delete temperature.js

--- temperature.js
+++
@@ -1,8 +0,0 @@
-function temperatureConverter() {
-    valNum = document.getElementById("tempInput").value
-    console.log(parseInt(valNum),"valueeeee")
-    document.getElementById("outputCelsius").innerHTML =  (valNum-32) / 1.8;
-    event.preventDefault()
-  }
-
-
